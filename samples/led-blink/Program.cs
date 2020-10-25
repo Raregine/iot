54fb1ea1709ca6b3212fef5a4f6f38bdc8caf58e
raregine <ray.regine@gmail.com>
2020-10-25 16:45:41 +0000

    added device twin code - no changes

--- samples/led-blink/Program.cs
+++ samples/led-blink/Program.cs
@@ -5,21 +5,66 @@
 using System;
 using System.Device.Gpio;
 using System.Threading;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace led_blink
 {
     class Program
     {
+        //device twin code from https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-csharp-csharp-module-twin-getstarted
+        private const string ModuleConnectionString = "HostName=dev-iotsolution-iothub.azure-devices.net;DeviceId=EdgePi;ModuleId=ledblink;SharedAccessKey=EQ84KKRsUrCUfuOb3lpolFd0vg/y/VHbSOtf8OU/g+Y=";
+        private static ModuleClient Client = null;
+        static void ConnectionStatusChangeHandler(ConnectionStatus status,
+          ConnectionStatusChangeReason reason)
+        {
+            Console.WriteLine("Connection Status Changed to {0}; the reason is {1}",
+              status, reason);
+        }
         static void Main(string[] args)
         {
             var pin = 18;
             var lightTime = 1000;
             var dimTime = 200;
-            
+
             Console.WriteLine($"Let's blink an LED!");
             using GpioController controller = new GpioController();
             controller.OpenPin(pin, PinMode.Output);
             Console.WriteLine($"GPIO pin enabled for use: {pin}");
+
+            //Start Device twin code from https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-csharp-csharp-module-twin-getstarted 
+            Microsoft.Azure.Devices.Client.TransportType transport = Microsoft.Azure.Devices.Client.TransportType.Amqp;
+
+            try
+            {
+                Client =
+                  ModuleClient.CreateFromConnectionString(ModuleConnectionString, transport);
+                Client.SetConnectionStatusChangesHandler(ConnectionStatusChangeHandler);
+                Client.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertyChanged, null).Wait();
+
+                Console.WriteLine("Retrieving twin");
+                var twinTask = Client.GetTwinAsync();
+                twinTask.Wait();
+                var twin = twinTask.Result;
+                Console.WriteLine(JsonConvert.SerializeObject(twin.Properties));
+
+                Console.WriteLine("Sending app start time as reported property");
+                TwinCollection reportedProperties = new TwinCollection();
+                reportedProperties["DateTimeLastAppLaunch"] = DateTime.Now;
+
+                Client.UpdateReportedPropertiesAsync(reportedProperties);
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine("Error in sample: {0}", ex);
+            }
+
+            Console.WriteLine("Waiting for Events.  Press enter to exit...");
+            Console.ReadLine();
+            Client.CloseAsync().Wait();
+            // End device twin code from https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-csharp-csharp-module-twin-getstarted
 
             // turn LED on and off
             while (true)
@@ -33,5 +78,21 @@ namespace led_blink
                 Thread.Sleep(dimTime);
             }
         }
+
+        //device twin code from https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-csharp-csharp-module-twin-getstarted
+        private static async Task OnDesiredPropertyChanged(TwinCollection desiredProperties, object userContext)
+        {
+            Console.WriteLine("desired property change:");
+            Console.WriteLine(JsonConvert.SerializeObject(desiredProperties));
+            Console.WriteLine("Sending current time as reported property");
+            TwinCollection reportedProperties = new TwinCollection
+            {
+                ["DateTimeLastDesiredPropertyChangeReceived"] = DateTime.Now
+            };
+
+            await Client.UpdateReportedPropertiesAsync(reportedProperties).ConfigureAwait(false);
+        }
     }
+
+
 }
